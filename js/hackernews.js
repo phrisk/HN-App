508755bb7b4dbd15c34d4f454b9743e024f869a6
sshannon <stevieshannon@gmail.com>
2012-09-24 15:16:07 +0100

    Added basic comments view and back button

--- js/hackernews.js
+++ js/hackernews.js
@@ -7,6 +7,13 @@ $('#HNmainView').live('pageinit',function(event){
 	
 });
 
+// Listen for comment link to be clicked
+$(".commentLink").live("click", function(evt){
+
+    showComments( $(this).attr('id') );
+    
+});
+
 //////////////////////////////////////////////////
 //	Main Application Functions
 //////////////////////////////////////////////////
@@ -14,33 +21,61 @@ $('#HNmainView').live('pageinit',function(event){
 // Show the News Items
 var showNews = function(){
 
-	// Get the Raw JSON from the HackerNews API
-	$.getJSON(serverAddress + "/links?callback=?",
+	// Get the Raw JSON from the HN-API
+	var showNewsCall = $.getJSON(serverAddress + "/links?callback=?",
 	  function(data) {
 	  	// Process each of the JSON items, appending them to the list view
 	    $.each(data.items, function(i,item){
 
-	      $("#HNcontentarea").append("<li><a href='" + item.href + "'><h3>" + item.title + "</h3><p>" + item.points + " points by " + item.by + " " + item.date + " ago | " + item.comments + " comments</p><span class='ui-li-count'>" + item.points + "</span></a><a href='javascript:showComments(" + item.postid + ");'></a></li>");
+	      $("#HNcontentarea").append("<li><a href='" + item.href + "'><h3>" + item.title + "</h3><p>" + item.points + " points by " + item.by + " " + item.date + " ago | " + item.comments + " comments</p><span class='ui-li-count'>" + item.points + "</span></a><a href='#' id='" + item.postid + "' class='commentLink'></a></li>");
 
 	    });
-	  }).complete(function() { $("#HNcontentarea").listview("refresh"); });
-	  // Once complete, refresh the list view to update the styling
+	  });
+
+	// Once complete, refresh the list view to update the styling
+	showNewsCall.complete(function() { $("#HNcontentarea").listview("refresh"); });
 
 };
 
-/* Take the clicked post's ID and use it to retrieve the post comments etc
+// Take the clicked post's ID and use it to retrieve the post: title, comments etc
+// Load in a template for the view and populate it with the JSON data
 var showComments = function( postID ){
 
-	// Get the Raw JSON from the HackerNews API
-	$.getJSON("http://api.ihackernews.com/post/" + postID,
-	  function(data) {
-	  	// Process each of the JSON items, appending them to the list view
-	    $.each(data.items, function(i,item){
+	var commentView = "<div data-role='page' id='commentsView'>" +
+					  "<div data-role='header' id='commentsHeader'>" +
+					  "<a href='#HNmainView' class='ui-btn-left' data-icon='arrow-l'>Back</a>" +
+					  "</div>" +
+					  "<ul data-role='listview' data-inset='false' id='comments'>" +
+					  "</ul>" +
+					  "</div>";
 
-	      $("#HNcontentarea").append("<li><a href='" + item.url + "'><h3>" + item.title + "</h3><p>" + item.points + " points by " + item.postedBy + " " + item.postedAgo + " | " + item.commentCount + " comments</p><span class='ui-li-count'>" + item.points + "</span></a><a href='http://news.ycombinator.com/item?id=" + item.id + "'>Purchase album</li>");
+	// If there is an old commentView, remove it
+	if( $("#commentsView") )
+		$("#commentsView").remove();
+
+	// Get the Raw JSON from the HN-API
+	var showCommentsCall = $.getJSON(serverAddress + "/post/" + postID + "?callback=?",
+	  function(data) {
+
+	  	// First load the comments view template
+	  	$("body").append(commentView);
+
+	  	// Add title to view
+	  	$("#commentsHeader").append("<h1>" + data.post.title + "</h1>");
+
+	  	// Process each of the JSON items, appending them to the list view
+	    $.each(data.comments, function(i,item){
+
+	      $("#comments").append("<li>" + item.text + "</li>");
 
 	    });
-	  }).complete(function() { $("#HNcontentarea").listview("refresh"); });
-	  // Once complete, refresh the list view to update the styling
 
-}; */
+	    // Switch to comments view
+	    window.location.replace("#commentsView");
+
+	  });
+
+	// Once complete, refresh the list view to update the styling
+	showCommentsCall.complete(function() { $("#comments").listview("refresh"); });
+
+};
